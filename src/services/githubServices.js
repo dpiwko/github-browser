8d3d761720d08841c7b6e070f526e49d94691369
Dawid Piwko <dpiwko@gmail.com>
2020-08-11 18:14:37 +0200

    Updates

--- src/services/githubServices.js
+++
@@ -1,9 +0,0 @@
-export const getGithubProfile = (userName) => {
-    return fetch(`https://api.github.com/users/${userName}`)
-        .then((response) => response.json())
-  }
-  
-export const getGithubHistory = (userName) => {
-  return fetch(`https://api.github.com/users/${userName}/events/public?per_page=50`)
-    .then((response) => response.json())
-}
